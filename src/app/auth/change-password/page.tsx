28e2d1d8874439b577d90c89c2064045be40e5b4
edm-web <mmgeaindimoe@gmail.com>
2026-06-19 07:25:23 +0630

    change password suspense updated

--- src/app/auth/change-password/page.tsx
+++ src/app/auth/change-password/page.tsx
@@ -1,7 +1,13 @@
+import Loading from "@/app/posts/[id]/loading";
 import ChangePasswordForm from "@/features/auth/components/change-password-form";
+import { Suspense } from "react";
 
 const ChangePasswordPage = () => {
-  return <ChangePasswordForm />;
+  return (
+    <Suspense fallback={<Loading />}>
+      <ChangePasswordForm />
+    </Suspense>
+  );
 };
 
 export default ChangePasswordPage;
