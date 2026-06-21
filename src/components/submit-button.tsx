5c8590facab2f3bc2ba05c23562dbbff93141880
edm-web <mmgeaindimoe@gmail.com>
2026-06-21 10:16:02 +0630

    pudate submit btn flex

--- src/components/submit-button.tsx
+++ src/components/submit-button.tsx
@@ -1,18 +1,16 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { LoaderCircle } from "lucide-react";
-import { useFormStatus } from "react-dom";
 
 type SubmitButtonProp = {
   label: string;
   isPending: boolean;
 };
 const SubmitButton = ({ label, isPending }: SubmitButtonProp) => {
-  const { pending } = useFormStatus();
   return (
     <Button type="submit" disabled={isPending} className="cursor-pointer">
       {isPending ? (
-        <div>
+        <div className="flex gap-1">
           <LoaderCircle className="w-4 h-4 animate-spin" /> <span>{label}</span>
         </div>
       ) : (
